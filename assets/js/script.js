97c15834599feb3558a95349ce3ec42f3257c646
brerfox <brerfox33@gmail.com>
2020-01-07 02:23:38 +0300

    add preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+function preloader (){
+  $(() => {
+    setInterval(() => {
+      let p = $('.preloader');
+      p.css('opacity', 0);  
+      setInterval(
+        () => p.remove(),
+        parseInt(p.css('--duration')) * 1000
+      );
+    }, 1000)  
+  }); 
+};
+
+preloader();
+
 $('.nav-item').on('click', function(){
   var a = $('.nav-item.current');
   if (a) {
